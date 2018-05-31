f69d0a20f8e2dd8d42eb04e8cb5f464237254a39
Casper Heringa <5be8141b@opayq.com>
2018-05-31 10:36:16 +0200

    new repository

---
+++ Assets/scripts/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class MenuManager : MonoBehaviour {
+
+    public void SceneSwitch(){
+        SceneManager.LoadScene("Level");
+}
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
